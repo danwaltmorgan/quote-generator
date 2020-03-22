226a1a0237a21ba59fe1caf74e3cc2afabf2b2d9
Dan Morgan <ltdan681@gmail.com>
2020-03-22 17:09:15 -0400

    Updates

---
+++ precache-manifest.1089ed3b59b3b4cb6a75956062530218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee776d47d6c27e2767dfed29bfd7520c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90ca14911003a018a24",
+    "url": "/static/css/main.0debfa4e.chunk.css"
+  },
+  {
+    "revision": "64c04b1168b30af1822a",
+    "url": "/static/js/2.1aa91994.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1aa91994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90ca14911003a018a24",
+    "url": "/static/js/main.33ddee91.chunk.js"
+  },
+  {
+    "revision": "b67c4d86ebe46abe5888",
+    "url": "/static/js/runtime-main.b6bde8af.js"
+  }
+]);
