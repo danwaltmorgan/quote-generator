3d2af9ce74930669fdfa8406b8366e3a23619be3
Dan Morgan <ltdan681@gmail.com>
2020-03-22 17:54:28 -0400

    Updates

---
+++ precache-manifest.09e480ab314920e0955b7a6c9dd09418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dc8d50a36a763e79fba33395550043",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "c90ca14911003a018a24",
+    "url": "/quote-generator/static/css/main.0debfa4e.chunk.css"
+  },
+  {
+    "revision": "64c04b1168b30af1822a",
+    "url": "/quote-generator/static/js/2.1aa91994.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.1aa91994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90ca14911003a018a24",
+    "url": "/quote-generator/static/js/main.33ddee91.chunk.js"
+  },
+  {
+    "revision": "918787dd329722ebc4dc",
+    "url": "/quote-generator/static/js/runtime-main.1b0291d8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2feecb6296a273fc1c3dd30c161026dd.js"
+  "/quote-generator/precache-manifest.09e480ab314920e0955b7a6c9dd09418.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quote-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
